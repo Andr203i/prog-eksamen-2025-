c4e42fca3b3475f798f16d6924f133fcdb7071eb
StorePatter <cawe24ae@student.cbs.dk>
2025-03-20 09:13:20 +0100

    Create app.js
    Oprettelse af app.js

---
+++ invisteringplatform/js/app.js
@@ -0,0 +1,11 @@
+/*
+const express = require("express"); 
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static("Public")); 
+app.set("view engine", "ejs");
+*/
